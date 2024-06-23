6770b646cac4b174b2b9a049fac04200e5db0667
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-22 21:24:22 -0500

    Rediseño y nuevas migraciones.

---
+++ WEBAplicacionDeGestionDeCitasMedicas/Helpers/AuthenticatedRouteConstraint.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace WEBAplicacionDeGestionDeCitasMedicas.Helpers
+{
+    public class AuthenticatedRouteConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return httpContext.User.Identity.IsAuthenticated;
+        }
+    }
+}
